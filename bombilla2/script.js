f86c024b74d22a76ff4b06bb8d89242203b0c275
samujl06 <samueljl2006a@gmail.com>
2026-04-27 19:26:30 +0200

    Add files via upload

---
+++ bombilla2/script.js
@@ -0,0 +1,18 @@
+//Codigo Bombilla
+
+let encendida = false;
+
+const img = document.getElementById("bombilla");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  if (encendida) {
+    img.src = "apagada.png";
+    btn.textContent = "Encender";
+    encendida = false;
+  } else {
+    img.src = "encendida.png";
+    btn.textContent = "Apagar";
+    encendida = true;
+  }
+});   
